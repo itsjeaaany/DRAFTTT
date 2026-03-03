912a02eea2d7ad4978067b6da2820244157afb9c
Jeany Anne Butalid <j.butalid.553781@umindanao.edu.ph>
2026-03-03 17:25:52 +0800

    Update delete.php

--- PHPPDO/delete.php
+++ PHPPDO/delete.php
@@ -1,10 +1,14 @@
 <?php
 require 'config.php';
 
-if (isset($_GET['delete'])){
-    $user_id = $_GET['delete'];
+if (isset($_GET['users_id'])) {
+    $user_id = $_GET['users_id'];
 
-    $stmt = $pdo->prepare("DELETE FROM users WHERE user_id = ?");
+    $stmt = $pdo->prepare("DELETE FROM users WHERE users_id = ?");
     $stmt->execute([$user_id]);
+
+
+    header('Location: landing.php?deleted');
+    exit;
 }
 ?>
